e67231f01e4a11e22e028d5f4b1ad85a017b759f
Crux <3271348035@qq.com>
2019-10-16 19:52:07 +0800

    nettyclient

---
+++ src/main/java/com/huatec/edu/client/App.java
@@ -0,0 +1,13 @@
+package com.huatec.edu.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/huatec/edu/client/proxy/NettyClientProxy.java
@@ -0,0 +1,5 @@
+package com.huatec.edu.client.proxy;
+
+public class NettyClientProxy {
+
+}
